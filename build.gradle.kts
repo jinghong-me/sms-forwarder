f17127dda016400f4e03c342eecaccd5d3f0276d
灯火通明 <lanbing89@gmail.com>
2026-03-17 15:07:49 +0800

    Fix AGP version and remove offline mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // 保持 AGP 版本不变（如果你需要可以调整），将 Kotlin 插件升级为 1.9.20
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
